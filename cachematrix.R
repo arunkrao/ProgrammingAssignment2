5a69d834caee132eb42515c3c7bdbbeed83bbae3
arunkrao <arunrao@hotmail.com>
2015-02-16 12:06:25 +0530

    Update cachematrix.R
    Programming assignment completed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,77 @@
-## These pair of functions enable caching of matrix inverses so that repeated retrievals of matrix inverses are efficient
-## 
+## The set of functions in this file allow caching of matrix inverses
+## Since inverses of large matrices can take significant computational time,
+## it is more efficient to store the inverse the first time it is computed
+## and only fetch it from the cache in subsequent requests for the inverse
 
-## makeCacheMatrix lets us cache the inverse of a matrix so that we do not have to repeatedly spend time computing it
+## This function caches the matrix and its inverse and returns a list 
+## of function for getting and setting the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-	xinverse <- NULL
-	set <- function (y) {
-		x <<- y
-		xinverse <<- NULL
-	}
-	get <- function () x
-	setinverse <- function (inv) xinverse <<- inv
-	getinverse <- function () xinverse
-	list (set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  matInverse <- NULL
+  setMatrix <- function (y)
+  {
+    x <<- y
+    matInverse <<- NULL
+  }
+  
+  getMatrix <- function () x
+  
+  setInverse <- function (inv)   matInverse <<- inv
+  
+  getInverse <- function () matInverse
+  
+  list (s = setMatrix, g = getMatrix, sI = setInverse, gI = getInverse)
 }
 
 
-## cacheSolve checks if the inverse of the matrix is available in the cache, if so returns it, else computes, populates ..
-##   .. the cache and then returns the inverse
+## cacheSolve returns the inverse for a cached matrix, computing it if 
+## its the first time that the inverse of the matrix is being requested
 
 cacheSolve <- function(x, ...) {
- 	inv <- x$getinverse ()
-	if (!is.null (inv)) {
-		message ("getting cached inverse of the matrix")
-		return (inv)
-	}
-	mat <- x$get ()
-	inv <- solve (mat)
-	x$setinverse (inv)	
-	inv
-       ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$gI()
+  
+  if (!is.null (inv))
+  {
+    message ("Using the cache")
+    return (inv)
+  }
+  
+  inv = x$sI (solve (x$g()))
+  inv
 }
 
-## Not part of the functions, test code to check if the above functions work correctly or not
-m <- matrix (1:4, nrow = 2, ncol = 2)           # Make a test matrix  ...
-m2 <- makeCacheMatrix (m)	                # Create a cache for it ...
-cacheSolve (m2)                                 # Get the inverse - first time - should compute & store in cache
-cacheSolve (m2)                                 # Get the inverse again - should use the cache this time
+## A test function to see if the matrix caching functions are working correctly 
+## In order to avoid printing a 5000 by 5000 matrix, printing a single computed
+## number based on all the rows and columns of the inverted matrix.
+## The computed number should be the same in each call and the first call is the
+## only one that should take significant time
+
+test <- function ()
+{
+  mat <- matrix (rnorm (25000000), 5000, 5000)
+  cache <- makeCacheMatrix (mat)
+  print (system.time (iv <- cacheSolve (cache)))
+  print (mean (rowSums (iv)))
+  print (system.time (iv <- cacheSolve (cache)))
+  print (mean (rowSums (iv)))
+  print (system.time (iv <- cacheSolve (cache)))
+  print (mean (rowSums (iv)))
+  
+}
+
+## Below are the test results when I run it on my machine
+
+#   > test ()
+#   user  system elapsed 
+#   96.44    0.13   96.68 
+#   [1] -0.002816375
+#   Using the cache
+#   user  system elapsed 
+#   0       0       0 
+#   [1] -0.002816375
+#   Using the cache
+#   user  system elapsed 
+#   0       0       0 
+#   [1] -0.002816375
+#   > 
